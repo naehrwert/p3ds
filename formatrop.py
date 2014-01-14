d92b9110e4319f0cd8a56a29e149af3eac887b14
naehrwert <lv0loader@gmail.com>
2014-01-14 20:32:16 +0100

    Cleanup.

---
+++ formatrop.py
@@ -0,0 +1,45 @@
+# Hackityhack ROP formatter.
+# Copyright (C) 2013 naehrwert
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 2.0.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License 2.0 for more details.
+# 
+# A copy of the GPL 2.0 should have been included with the program.
+# If not, see http://www.gnu.org/licenses/
+
+import struct
+import darm # https://github.com/jbremer/darm
+
+# Read RAM dump.
+f = open("DUMP.BIN")
+dump = f.read()
+f.close()
+
+# Read decrypted launcher.
+f = open("Launcher.dat", "rb")
+data = f.read()
+f.close()
+
+# Format entries.
+for i in xrange(len(data)/4):
+	v = struct.unpack("<I", data[i*4:i*4+4])[0]
+	inst = ""
+	if v >= 0x100000 and v <= 0x252000: # This is gonna work just fine.
+		if v & 1:
+			addr = (v - 0x100000) & 0xFFFFFFFE
+			inst = darm.disasm_thumb(struct.unpack("<H", dump[addr:addr+2])[0])
+		else:
+			addr = v - 0x100000
+			inst = darm.disasm_armv7(struct.unpack("<I", dump[addr:addr+4])[0])
+		if inst == None:
+			print "{0:08X}: {1:08X}".format(i*4, v)
+		else:
+			print "{0:08X}: {1:08X} - {2}".format(i*4, v, inst)
+	else:
+		print "{0:08X}: {1:08X}".format(i*4, v)
--- ramdump.py
+++ ramdump.py
@@ -13,37 +13,36 @@
 # A copy of the GPL 2.0 should have been included with the program.
 # If not, see http://www.gnu.org/licenses/
 
+import sys
+
 from p3ds.util import *
 from p3ds.ROP import *
 
-r = ROP(0x002B0000)
+def main(argv):
+	r = ROP(0x002B0000)
 
-# Clear 0x279004 and 0x279008
+	# Clear 0x279004 and 0x279008
+	r.store_i32(0, 0x279004)
+	r.store_i32(0, 0x279008)
 
-#r.pop_r4(0x279004)
-#r.pop_r1(0x279008)
-#r.i32(0x101AAC)
-#r.i32(0x000004)
+	# file_open(0x279000, "YS:/DUMP.BIN", 6)
+	r.call(0x1B82AC, [0x279000, Ref("fname"), 6], 5)
+	# file_write(0x279000, 0x279020, 0x100000, 0x300000)
+	r.call(0x1B3B54, [0x279000, 0x279020, 0x100000, 0x300000], 9)
 
-#r.pop_r1(0)
-#r.store_r1(0x279004)
-#r.store_r1(0x279008)
+	# Data.
+	r.label("fname")
+	r.data(
+	"\x59\x00\x53\x00\x3A\x00\x2F\x00\x44\x00\x55\x00\x4D\x00\x50\x00"
+	"\x2E\x00\x42\x00\x49\x00\x4E\x00\x00\x00")
 
-r.store_i32(0, 0x279004)
-r.store_i32(0, 0x279008)
+	rop = r.gen()
+	
+	#hexdump(rop, base=0x2B0000)
 
-# file_open(0x270000, "YS:/DUMP.BIN", 6)
-r.call(0x1B82AC, [0x279000, Ref("fname"), 6], 5)
-# file_write(0x270000, 0x279020, 0x100000, 0x300000)
-r.call(0x1B3B54, [0x279000, 0x279020, 0x100000, 0x300000], 9)
+	f = open(argv[0], "wb")
+	f.write(rop)
+	f.close()
 
-# Data.
-r.label("fname")
-r.data("\x59\x00\x53\x00\x3A\x00\x2F\x00\x44\x00\x55\x00\x4D\x00\x50\x00\x2E\x00\x42\x00\x49\x00\x4E\x00\x00\x00")
-
-rop = r.gen()
-hexdump(rop, base=0x2B0000)
-
-#f = open("Launcher.dat", "wb")
-#f.write(rop)
-#f.close()
+if __name__ == "__main__":
+	main(sys.argv[1:])
